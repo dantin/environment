91f8fe9d1ac5f10caaf20170e30b34f139528c4b
David Ding <chengjie.ding@gmail.com>
2014-12-30 13:39:32 +0800

    add script and configuration sync script

---
+++ bin/auto_sync.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+TO_DIR="/home/david/Documents/code/projects/environment"
+
+BIN_DIR="/home/david/.bin"
+NGINX_HOME="/usr/local/share/nginx"
+
+# copy script utility in ~/.bin directory
+cp $BIN_DIR/*.sh $TO_DIR/bin
+cp $BIN_DIR/goto $TO_DIR/bin
+
+# Nginx
+cp $NGINX_HOME/conf/nginx.conf $TO_DIR/config
+cp /etc/init.d/nginx $TO_DIR/etc/init.d
+
