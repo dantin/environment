130b2430e6c85a9448b09b3a41f65989a4723c84
David Ding <chengjie.ding@gmail.com>
2015-01-14 15:13:46 +0800

    add mutiple directory support

--- bin/rssh.sh
+++ bin/rssh.sh
@@ -3,65 +3,89 @@
 # 加载公共函数脚本
 source /home/david/.bin/config.sh
 
+CONFIG="/home/david/Documents/work/fangdd/hosts.config"
+SCRIPT_NAME=`basename $0`
+
 # 自动登录函数
 #
 # 参数：
 # password 密码
 # host     远程机器，格式'user@host'
+# target   登录目标目录，可选
 #
 auto_login_ssh(){
     # StrictHostKeyChecking=no参数让ssh默认添加新主机的公钥指纹
     # 防止出现是否继续yes/no的提示
-    expect -c "
-        set timeout -1;
-        spawn -noecho ssh -o StrictHostKeyChecking=no $2 ${@:3};
-        expect *assword:*;
-        send -- $1\r;
-        interact;"
+    if [ -z $3 ]; then
+        echo "GOTO \$HOME"
+        expect -c "
+            set timeout -1;
+            spawn -noecho ssh -o StrictHostKeyChecking=no $2 ${@:3};
+            expect *assword:*;
+            send -- $1\r;
+            interact;"
+    else
+        echo "GOTO $3"
+        expect -c "
+            set timeout -1;
+            spawn -noecho ssh -o StrictHostKeyChecking=no $2;
+            expect *assword:*;
+            send -- $1\r;
+            expect *~*;
+            send -- \"cd $3\n\";
+            interact;"
+    fi
+}
+
+get_location(){
+    if [ -z $2 ]; then
+        echo ""
+    else
+        case $2 in
+            app)
+                echo $(load_config_key $CONFIG "$1.app")
+                ;;
+            log)
+                echo $(load_config_key $CONFIG "$1.log")
+                ;;
+        esac
+        echo ""
+    fi
 }
 
 # Main函数
 #
 HOST=`echo $1 | tr '[:upper:]' '[:lower:]'`
-CONFIG="/home/david/Documents/work/fangdd/hosts.config"
-SCRIPT_NAME=`basename $0`
 
 case ${HOST} in
     21)
         Hostname=$(load_config_key $CONFIG "21.hostname")
         Username=$(load_config_key $CONFIG "21.username")
         Password=$(load_config_key $CONFIG "21.password")
-
-        auto_login_ssh "$Password" "$Username@$Hostname"
+        Dir=$(get_location "21" $2)
         ;;
     78)
         Hostname=$(load_config_key $CONFIG "78.hostname")
         Username=$(load_config_key $CONFIG "78.username")
         Password=$(load_config_key $CONFIG "78.password")
-
-        auto_login_ssh "$Password" "$Username@$Hostname"
+        Dir=$(get_location "78" $2)
         ;;
     151)
         Hostname=$(load_config_key $CONFIG "151.hostname")
         Username=$(load_config_key $CONFIG "151.username")
         Password=$(load_config_key $CONFIG "151.password")
-
-        auto_login_ssh "$Password" "$Username@$Hostname"
-        ;;
-    gw)
-        Hostname=$(load_config_key $CONFIG "gw.hostname")
-        Username=$(load_config_key $CONFIG "gw.username")
-        Password=$2
-        if [ -z "$Password" ]
-        then
-            echo "Password can not be empty!"
-        fi
-
-        #auto_login_ssh "$Password" "$Username@$Hostname"
+        Dir=$(get_location "151" $2)
         ;;
     *)
         echo
-        echo "Usage: $SCRIPT_NAME {21|78|151|gw <key>}"
+        echo "Usage: $SCRIPT_NAME {21|78|151} [app|log]"
+        exit 1
         ;;
 esac
+
+if [ -z $Dir ]; then
+    auto_login_ssh "$Password" "$Username@$Hostname"
+else
+    auto_login_ssh "$Password" "$Username@$Hostname" "$Dir"
+fi
 
